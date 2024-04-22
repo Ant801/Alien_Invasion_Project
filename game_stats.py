0bb7cfe01d51f3f4c4ff11d5affc378ce10f8a6a
Ant801 <ant8011@gmail.com>
2024-04-22 16:58:48 +0100

    Alien Ship collision reaction

---
+++ game_stats.py
@@ -0,0 +1,10 @@
+
+class GameStats:
+    def __init__(self, ai_game):
+        """Initilise statistics."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialise statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
