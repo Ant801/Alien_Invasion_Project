5fade605e5c9319717cffd8d5b6fa2d487d782ed
Ant801 <ant8011@gmail.com>
2024-04-23 12:43:53 +0100

    Bugfix - play button & Hiding mouse

--- main.py
+++ main.py
@@ -87,7 +87,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
             # Reset the game statistics
             self.stats.reset_stats()
             self.game_active = True
@@ -99,6 +100,9 @@ class AlienInvasion:
             # Create a new fleet and center the ship
             self._create_fleet()
             self.ship.center_ship()
+
+            # Hide the mouse cursor
+            pygame.mouse.set_visible(False)
 
     def _fire_bullet_(self):
         """Create a new bullet and add it to the bullets group."""
@@ -209,6 +213,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
